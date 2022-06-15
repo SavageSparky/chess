d6b271a90778021a45da2fe8c8e62a56cd292e56
Akilan <akilan73210@gmail.com>
2022-06-15 16:53:09 +0530

    removed old code

--- pieces/Bishop.java
+++ pieces/Bishop.java
@@ -4,45 +4,15 @@ import chess.exceptions.*;
 import chess.board.Board;
 
 public class Bishop extends ChessPiece {
-  // private int rowPosition;
-  // private int columnPosition;
-  // private boolean isTeamWhite;
-
   private ChessPieceType chessPieceType = ChessPieceType.BISHOP;
-  // private boolean isBishopWhite;
 
   public Bishop(int rowPosition, int columnPosition, boolean isTeamWhite){
     super(rowPosition, columnPosition, isTeamWhite);
-    // this.isBishopWhite = isBishopWhite;
   }
 
-  // public Bishop(int rowPosition, int columnPosition, boolean isTeamWhite){
-  //   this.rowPosition = rowPosition;
-  //   this.columnPosition = columnPosition;
-  //   this.isTeamWhite = isTeamWhite;
-  // }
-
-  // // getters
-  // @Override
-  // public int getRowPosition() {
-  //   return rowPosition;
-  // }
-  // @Override
-  // public int getColumnPosition() {
-  //   return columnPosition;
-  // }
-  // @Override
-  // public boolean isTeamWhite() {
-  //   return isTeamWhite;
-  // }
   public ChessPieceType getChessPieceType(){
     return chessPieceType;
   }
-
-  // // Bishop specific getter
-  // public boolean isBishopWhite(){
-  //   return isBishopWhite;
-  // }
 
   // Bishop specific functionalities
   @Override
